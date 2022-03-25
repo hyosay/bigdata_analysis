a0e61cdabba54b4db3c1c50c1cb0f1d505156dae
Hyoseong Jeon <gytjdwjs@cu.ac.kr>
2022-03-25 10:31:30 +0900

    first

---
+++ Beautifulsoup.py
@@ -0,0 +1,11 @@
+'''def solution(participant, completion):
+
+    for i in range
+        answer = ''
+    for i in participant:
+        if i in completion:
+
+    return answer'''
+
+table = [0 for i in range(10)]
+print(table)
